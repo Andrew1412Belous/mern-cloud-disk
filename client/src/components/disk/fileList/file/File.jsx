c0019454d5be2da08a74705eeda08a0e7d4150a1
Andrew1412Belous <andrew.belous1412@gmail.com>
2023-06-01 18:41:23 +0300

    add downloading files

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { pushToStack, setCurrentDir } from '../../../../reducers/fileReducer'
 
 import './file.scss'
+import { downloadFile } from '../../../../actions/files'
 
 const File = ({ file }) => {
   const dispatch = useDispatch()
@@ -17,12 +18,21 @@ const File = ({ file }) => {
     }
   }
 
+  function downloadClickHandler (event) {
+    event.stopPropagation()
+
+    downloadFile(file)
+  }
+
   return (
     <div className='file' onClick={() => openDirHandler(file)}>
       <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
       <div className="file__name">{file.name}</div>
       <div className="file__date">{file.date.slice(0,10)}</div>
       <div className="file__size">{file.size}</div>
+      {file.type !== 'dir' &&
+        <button className="file__btn file__download" onClick={(event) => downloadClickHandler(event)}>Download</button>}
+      <button className="file__btn file__delete">Delete</button>
     </div>
   );
 };
